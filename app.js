af2ba231876530f739e5b42c64356641bbd7a2b6
Lee <leewhiz119@gmail.com>
2022-09-28 12:14:21 -0400

    installed ejs converted views to ejs and rendered index.ejs

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express=require('express')
 
 const app=express()
 
+//register user engine
+app.set('view engine', 'ejs')
+
 app.listen(3000, ()=>console.log('express app listening on port 3000'))
 
 app.get('/', (req, res)=>{
-    res.sendFile('views/index.html', { root: __dirname})
+    res.render('index')
+    console.log('hello')
 })
 
 app.get('/about', (req, res)=>{
