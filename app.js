fe96e0072b9dbe8362e5d726094174a01ec35940
Lee <leewhiz119@gmail.com>
2022-09-28 11:16:19 -0400

    added routing to views

--- app.js
+++ app.js
@@ -5,5 +5,13 @@ const app=express()
 app.listen(3000, ()=>console.log('express app listening on port 3000'))
 
 app.get('/', (req, res)=>{
-    res.send('Hi guys!!!')
+    res.sendFile('views/index.html', { root: __dirname})
+})
+
+app.get('/about', (req, res)=>{
+    res.sendFile('views/about.html', { root: __dirname})
+})
+
+app.use('/', (req, res)=>{
+    res.sendFile('views/404.html', { root: __dirname})
 })
